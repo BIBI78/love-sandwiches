56881ca9b254e52286ec25395e0cee30e58da47b
BIBI78 <blakecollins78@gmail.com>
2023-01-06 18:24:46 +0000

    collect sales dat for the last 5 markest in lists of columns

--- run.py
+++ run.py
@@ -17,6 +17,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -50,64 +53,72 @@ def validate_data(values):
         print(f"Invalid data: {e}, please try again.\n")
         return False
 
-
     return True
 
 
-
-
-
-
-def update_worksheet(data,worksheet):
+def update_worksheet(data, worksheet):
     """
-    update worksheet recieves a list of integers to be inserted into a worksheet
-    updates the relavent worksheet with the data provided
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
-    print(f"updating {worksheet} worksheet\n")
-    worksheet_to_update= SHEET.worksheet(worksheet)
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated succesfullty\n")
-
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
     """
-
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
-    print("calculating surplus data")
-    stock= SHEET.worksheet("stock").get_all_values()
-    pprint(stock)
-    stock_row= stock[-1]
-    print(f"stock row: {stock_row}")
-    print(f"sales row:{sales_row}")
-
-
-    surplus_data= []
-    for stock,sales in zip(stock_row,sales_row):
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    # column = sales.col_values(3)
+    # print(column)
 
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns 
+
+
+   
 
 
 def main():
     """
-    run all program functions
+    Run all program functions
     """
-data = get_sales_data()
-print(data)
-sales_data= [int(num)for num in data]
-update_worksheet(sales_data,"sales")
-calculate_surplus_data(sales_data)
-new_surplus_data = calculate_surplus_data(sales_data)
-update_worksheet(sales_data,"surplus")
-
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+#main()
 
-
+get_last_5_entries_sales()
