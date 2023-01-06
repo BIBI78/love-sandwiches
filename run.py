c2d48741828c527bd1ccc2eb55a68252100a52e3
BIBI78 <blakecollins78@gmail.com>
2023-01-06 07:52:32 +0000

    add surplus data function

--- run.py
+++ run.py
@@ -73,7 +73,16 @@ def calculate_surplus_data(sales_row):
     stock= SHEET.worksheet("stock").get_all_values()
     pprint(stock)
     stock_row= stock[-1]
-    print(stock_row)
+    print(f"stock row: {stock_row}")
+    print(f"sales row:{sales_row}")
+
+
+    surplus_data= []
+    for stock,sales in zip(stock_row,sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 def main():
     """
@@ -81,9 +90,11 @@ def main():
     """
 data = get_sales_data()
 print(data)
-sales_data= [(num)for num in data]
+sales_data= [int(num)for num in data]
 update_sales_worksheet(sales_data)
 calculate_surplus_data(sales_data)
+new_surplus_data = calculate_surplus_data(sales_data)
+print(new_surplus_data)
 
 
 
