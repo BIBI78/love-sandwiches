11da0bff5b6b7d431530d3782891650d7766fd4e
BIBI78 <blakecollins78@gmail.com>
2023-01-06 17:01:12 +0000

    add refractoring to old sales and surplus functions, update worksheet function

--- run.py
+++ run.py
@@ -54,15 +54,20 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """
-    update sales worksheet and add new row with list data provided
 
+
+
+
+def update_worksheet(data,worksheet):
     """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated succefully\n")
+    update worksheet recieves a list of integers to be inserted into a worksheet
+    updates the relavent worksheet with the data provided
+    """
+    print(f"updating {worksheet} worksheet\n")
+    worksheet_to_update= SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated succesfullty\n")
+
 
 
 def calculate_surplus_data(sales_row):
@@ -85,15 +90,6 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def update_surplus_worksheet(data):
-    """
-    ????
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated succefully\n")
-
 
 
 
@@ -104,15 +100,14 @@ def main():
 data = get_sales_data()
 print(data)
 sales_data= [int(num)for num in data]
-update_sales_worksheet(sales_data)
+update_worksheet(sales_data,"sales")
 calculate_surplus_data(sales_data)
 new_surplus_data = calculate_surplus_data(sales_data)
-update_surplus_worksheet(sales_data)
+update_worksheet(sales_data,"surplus")
 
 
 
-
-print("Welcome to Love sandwiches data automation")
+print("Welcome to Love Sandwiches Data Automation")
 main()
 
 
