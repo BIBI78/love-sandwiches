4773f36bceeb94e026559705568fcde04a70fa05
BIBI78 <blakecollins78@gmail.com>
2023-01-14 17:17:41 +0000

    add lose and gain function to main function

--- gun.py
+++ gun.py
@@ -159,35 +159,57 @@ print("Your answer is: {}".format(answer))
 
 
 """
-
-
+#FUNCTION 5
+"""
+### ADD TRY EXCEPT BS###
 def weight_change():
     while True:
         weight_change = input("Would you like to lose or gain weight? (lose or gain): ")
         if weight_change == "lose":
-            lose_weight()
+            weight = float(input("Enter your current weight in kg: "))
+            desired_weight = float(input("Enter your desired weight in kg: "))
+            deficit_per_day = int(input("Enter your estimated calorie deficit per day: "))
+            time = weight_loss_time(weight, desired_weight, deficit_per_day) 
+            print(f"It will take approximately {time} days to reach your desired weight") 
+            weight_loss_time(weight, desired_weight, deficit_per_day)
             break
         elif weight_change == "gain":
-            gain_weight()
+            weight = float(input("Enter your current weight (in kg): "))
+            height = float(input("Enter your height (in cm): "))
+            age = int(input("Enter your age: "))
+            desired_weight = float(input("Enter your desired weight (in kg): "))
+            calories_per_day, time_to_reach_desired_weight = weight_gain_time(weight, height, age, desired_weight)
+            print("To gain weight, you should consume approximately", calories_per_day, "calories per day.")
+            print("It will take approximately {} weeks to reach your desired weight.".format(time_to_reach_desired_weight))
+            weight_gain_time(weight, height, age, desired_weight)
             break
         else:
             print("Invalid response, please enter either 'lose' or 'gain'")
 
-def lose_weight():
-    # code to calculate weight loss
-    print("You are in weight loss function")
+def weight_loss_time(weight, desired_weight, deficit_per_day):
+    weight_loss_rate = weight - desired_weight
+    #this eqn is from the net
+    deficit_needed = weight_loss_rate * 7700
+    days_needed = deficit_needed / deficit_per_day
+    return days_needed
 
-def gain_weight():
-    # code to calculate weight gain
-    print("You are in weight gain function")
+def weight_gain_time(weight, height, age, desired_weight):
+    # Harris-Benedict equation to calculate Basal Metabolic Rate (BMR)
+    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    # Multiply BMR by 1.55 to estimate calorie intake needed to gain weight
+    weight_gain_time = BMR * 1.55
+    # Estimate time to reach desired weight (assuming a weight gain of 0.5 kg per week)
+    time_to_reach_desired_weight = (desired_weight - weight) / 0.5
+    return weight_gain_time, time_to_reach_desired_weight
+
+
 
 weight_change()
- 
 
 
 
 
-
+""""
 
 main()
 
--- sketch.py
+++ sketch.py
@@ -232,7 +232,14 @@ def weight_change():
             weight_loss_time(weight, desired_weight, deficit_per_day)
             break
         elif weight_change == "gain":
-            gain_weight()
+            weight = float(input("Enter your current weight (in kg): "))
+            height = float(input("Enter your height (in cm): "))
+            age = int(input("Enter your age: "))
+            desired_weight = float(input("Enter your desired weight (in kg): "))
+            calories_per_day, time_to_reach_desired_weight = weight_gain_time(weight, height, age, desired_weight)
+            print("To gain weight, you should consume approximately", calories_per_day, "calories per day.")
+            print("It will take approximately {} weeks to reach your desired weight.".format(time_to_reach_desired_weight))
+            weight_gain_time(weight, height, age, desired_weight)
             break
         else:
             print("Invalid response, please enter either 'lose' or 'gain'")
@@ -244,18 +251,15 @@ def weight_loss_time(weight, desired_weight, deficit_per_day):
     days_needed = deficit_needed / deficit_per_day
     return days_needed
 
-    
+def weight_gain_time(weight, height, age, desired_weight):
+    # Harris-Benedict equation to calculate Basal Metabolic Rate (BMR)
+    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    # Multiply BMR by 1.55 to estimate calorie intake needed to gain weight
+    weight_gain_time = BMR * 1.55
+    # Estimate time to reach desired weight (assuming a weight gain of 0.5 kg per week)
+    time_to_reach_desired_weight = (desired_weight - weight) / 0.5
+    return weight_gain_time, time_to_reach_desired_weight
 
-    print("You are in weight loss function")
 
-
-
-
-
-
-
-def gain_weight():
-    # code to calculate weight gain
-    print("You are in weight gain function")
 
 weight_change()
