6205dd8543b79e06ef1d887d190177059ba117bc
BIBI78 <blakecollins78@gmail.com>
2023-01-23 11:14:41 +0000

    new sketch for bs

--- sketch.py
+++ sketch.py
@@ -6,260 +6,235 @@ from pprint import pprint
 import requests
 import json
 
-"""
-### edit this### this is the exercise function 
-def exercise_suggestion(age, sex, weight, height, desired_weight):
-    # Harris-Benedict equation to calculate Basal Metabolic Rate (BMR)
-    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-    # Multiply BMR by 1.55 to estimate calorie intake needed to gain weight
-    calorie_intake = BMR * 1.55
-    # Estimate time to reach desired weight (assuming a weight gain of 0.5 kg per week)
-    time_to_reach_desired_weight = (desired_weight - weight) / 0.5
-    #Calculate how much user should exercise per week
-    exercise_calories = (calorie_intake - BMR)/7
-    if sex == 'male':
-        exercise_suggestion = exercise_calories/77
-    else:
-        exercise_suggestion = exercise_calories/88
-    return exercise_suggestion
-
-sex = input("Enter your sex (male or female): ")
-age = int(input("Enter your age: "))
-weight = float(input("Enter your current weight (in kg): "))
-height = float(input("Enter your height (in cm): "))
-desired_weight = float(input("Enter your desired weight (in kg): "))
-
-exercise_per_week = exercise_suggestion(age, sex, weight, height, desired_weight)
-print("You should exercise approximately {:.2f} hours per week.".format(exercise_per_week))
-
-"""
-"""
-def diet_suggestion(age, weight, height):
-    # Harris-Benedict equation to calculate Basal Metabolic Rate (BMR)
-    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-    # Multiply BMR by 1.55 to estimate calorie intake needed to maintain weight
-    calorie_intake = BMR * 1.55
-    # Suggest diet based on age and calorie intake
-    if age < 30:
-        diet_suggestion = "Your suggested diet is {} calories per day with moderate exercise.".format(calorie_intake)
-    elif 30 <= age < 60:
-        diet_suggestion = "Your suggested diet is {} calories per day with regular exercise.".format(calorie_intake)
-    else:
-        diet_suggestion = "Your suggested diet is {} calories per day with regular exercise and a focus on nutrient-dense foods.".format(calorie_intake)
-    return diet_suggestion
-
-age = int(input("Enter your age: "))
-weight = float(input("Enter your current weight (in kg): "))
-height = float(input("Enter your height (in cm): "))
-
-suggested_diet = diet_suggestion(age, weight, height)
-print(suggested_diet)
-"""
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+from math import ceil
 
 
-"""
-def food_group_suggestion(preference):
-    # Suggest a food group based on user preference
-    if preference == 'vegetarian':
-        food_group_suggestion = 'Plant-based foods'
-    elif preference == 'low-carb':
-        food_group_suggestion = 'Protein and healthy fats'
-    elif preference == 'high-protein':
-        food_group_suggestion = 'Lean meats, eggs, and dairy'
-    else:
-        food_group_suggestion = 'A balanced mix of all food groups'
-    return food_group_suggestion
 
-preference = input("Enter your dietary preference (vegetarian, low-carb, high-protein, or none): ")
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-suggested_food_group = food_group_suggestion(preference)
-print("Your suggested food group is: {}.".format(suggested_food_group))
-"""
-"""
-### THIS IS THE DIET CODE ###
-# ok the first part works but i would like the FOOD group suggestion part to work too,
-# nested if statement ??
-def ask_question(question, valid_responses):
-    while True:
-        response = input(question)
-        if response in valid_responses:
-            if response == 'veg':
-                food_group_suggestion = 'Plant-based foods'
-            elif response == 'protein':
-                food_group_suggestion = 'eggs , lean meat, dairy'
-            elif response == 'carb':
-                food_group_suggestion = 'Protein and healthy fats'
-            elif response == 'none':
-                food_group_suggestion = 'A balanced mix of all food groups'
-            return response
-        else:
-            print("Invalid response, please enter one of the following: {}".format(valid_responses))
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('1000_sunny_fitness')
 
-question = "Enter your dietary preference (vegetarian, low-carb, high-protein, or none): "
-valid_responses = ["a", "b", "c", "d"]
+# Pirate flag #
+def draw_jolly_roger():
+    """
+    draws pirate flag
+    """
+    print("WELCOME TO THE 1000 SUNNY")
+    jolly_roger =  '.=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n'
+    jolly_roger += '|                     ______                     |\n'
+    jolly_roger += '|                  .-"      "-.                  |\n'
+    jolly_roger += '|                 /            \                 |\n'
+    jolly_roger += '|     _          |              |          _     |\n'
+    jolly_roger += '|    ( \         |,  .-.  .-.  ,|         / )    |\n'
+    jolly_roger += '|     > "=._     | )(__/  \__)( |     _.=" <     |\n'
+    jolly_roger += '|    (_/"=._"=._ |/     /\     \| _.="_.="\_)    |\n'
+    jolly_roger += '|           "=._"(_     ^^     _)"_.="           |\n'
+    jolly_roger += '|               "=\__|IIIIII|__/="               |\n'
+    jolly_roger += '|              _.="| \IIIIII/ |"=._              |\n'
+    jolly_roger += '|    _     _.="_.="\          /"=._"=._     _    |\n'
+    jolly_roger += '|   ( \_.="_.="     `--------`     "=._"=._/ )   |\n'
+    jolly_roger += '|    > _.="                            "=._ <    |\n'
+    jolly_roger += '|   (_/   free young thug                  \_)   |\n'
+    jolly_roger += '|                                                |\n'
+    jolly_roger += '.=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n'
+    print(jolly_roger)
 
-#suggested_food_group = food_group_suggestion(response)
-answer = ask_question(question, valid_responses)
-print("Your answer is: {}".format(answer))
-
-"""
-
-## WEATHER FUCNTION##
-
-"""
-## neeed an api key ##
-def get_weather(api_key, city_name):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
-    response = requests.get(url)
-    weather_data = json.loads(response.text)
-    if 'cod' not in weather_data or weather_data['cod'] != 200:
-        return None
-    else:
-        return weather_data
-
-api_key = "YOUR_API_KEY"
-city_name = "london"
-weather = get_weather(api_key, city_name)
-
-if weather is None:
-    print("Unable to get the weather for the given city")
-else:
-    print("Temperature in {} is {} degree celcius".format(city_name, weather['main']['temp']))
-
-
-"""
-
-    ####
-
-     #3
-"""
-# ok the first part works but i would like the FOOD group suggestion part to work too,
-# nested if statement ??
-def ask_question(question, valid_responses):
-    while True:
-        response = input(question)
-        if response in valid_responses:
-            if response == 'a':
-                food_group_suggestion = 'Plant-based foods'
-            elif response == 'protein':
-                food_group_suggestion = 'eggs , lean meat, dairy'
-            elif response == 'carb':
-                food_group_suggestion = 'Protein and healthy fats'
-            elif response == 'none':
-                food_group_suggestion = 'A balanced mix of all food groups'
-            return response
-        else:
-            print("Invalid response, please enter one of the following: {}".format(valid_responses))
-
-question = "Enter your dietary preference (vegetarian, low-carb, high-protein, or none): "
-valid_responses = ["a", "b", "c", "d"]
-
-#suggested_food_group = food_group_suggestion(response)
-answer = ask_question(question, valid_responses)
-print("Your answer is: {}".format(answer))
-"""
-####
-
-##############################
 
 #1
-"""
-weight = float(input("Enter your current weight in kg: "))
-desired_weight = float(input("Enter your desired weight in kg: "))
-deficit_per_day = int(input("Enter your estimated calorie deficit per day: "))
-time = weight_loss_time(weight, desired_weight, deficit_per_day)
-print(f"It will take approximately {time} days to reach your desired weight")  
-"""
-
+def weight_loss_time(weight, desired_weight,age,height):
+    """"
+    calculates users weight loss projection
+    """
+    weight_loss_rate = weight - desired_weight
+    deficit_needed = weight_loss_rate * 7700
+    recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
+    deficit_per_day = recomended_deficit 
+    days_needed = deficit_needed / deficit_per_day
+    recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
+    deficit_needed = recomended_deficit
+    #user_data = (weight,height,age,desired_weight)
+    return days_needed 
 #2
-"""
-# FUNCTION 2
-# NEED A WAY TO CHOSE WICH ONE TO CALL 
-# THE EQUATION IS A LITTLE FUCKED UP
-# FUNCTION FOR EXERCISE 
-# FUnction for  gain weight 
-weight = float(input("Enter your current weight (in kg): "))
-height = float(input("Enter your height (in cm): "))
-age = int(input("Enter your age: "))
-desired_weight = float(input("Enter your desired weight (in kg): "))
 
-calories_per_day, time_to_reach_desired_weight = weight_gain_time(weight, height, age, desired_weight)
-print("To gain weight, you should consume approximately", calories_per_day, "calories per day.")
-print("It will take approximately {} weeks to reach your desired weight.".format(time_to_reach_desired_weight))
- """
+def weight_gain_time(weight, height, age, desired_weight):
+    """
+    calculates ursers weigh gain projection
+    """
+    # this is the bmr for weight loss 
+    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    weight_gain_time = BMR * 1.55
+    time_to_reach_desired_weight = (desired_weight - weight) / 0.5
+    #user_data = (weight,height,age,desired_weight)
+    return weight_gain_time, time_to_reach_desired_weight
 
 
- #3
-"""
-# ok the first part works but i would like the FOOD group suggestion part to work too,
-# nested if statement ??
-def ask_question(question, valid_responses):
-    while True:
-        response = input(question)
-        if response in valid_responses:
-            if response == 'a':
-                print('Plant-based foods')
-            elif response == 'b':
-                print('eggs , lean meat, dairy')
-            elif response == 'c':
-                print('Protein and healthy fats')
-            elif response == 'd':
-                print('A balanced mix of all food groups')
-            return response
-        else:
-            print("Invalid response, please enter one of the following: {}".format(valid_responses))
-
-question = "Enter your dietary preference (vegetarian, low-carb, high-protein, or none): "
-valid_responses = ["a", "b", "c", "d"]
-
-#suggested_food_group = food_group_suggestion(response)
-answer = ask_question(question, valid_responses)
-print("Your answer is: {}".format(answer))
-
-
-"""
-
-### ADD TRY EXCEPT BS###
 def weight_change():
     while True:
-        weight_change = input("Would you like to lose or gain weight? (lose or gain): ")
+        weight_change = input("WELCOME TO 1000 SUNNY FITNESS \n Would you like to lose or gain weight? (lose or gain): \n")
         if weight_change == "lose":
-            weight = float(input("Enter your current weight in kg: "))
-            desired_weight = float(input("Enter your desired weight in kg: "))
-            deficit_per_day = int(input("Enter your estimated calorie deficit per day: "))
-            time = weight_loss_time(weight, desired_weight, deficit_per_day) 
-            print(f"It will take approximately {time} days to reach your desired weight") 
-            weight_loss_time(weight, desired_weight, deficit_per_day)
+            print("Please answer the following questions\n")
+            weight = float(input("Enter your current weight in kg:\n "))
+            height = float(input("Enter your height (in cm): \n"))
+            age = int(input("Enter your age: "))
+            desired_weight = float(input("Enter your desired weight in kg: \n"))
+            recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
+            time = weight_loss_time(weight, desired_weight,age,height)
+            print(f"You should eat about  {recomended_deficit} calories per day for about {time} days")
+            deficit_per_day = recomended_deficit 
+            weight_loss_time(weight, desired_weight,age,height)
+            user_data = (weight,height,age,desired_weight)
             break
         elif weight_change == "gain":
-            weight = float(input("Enter your current weight (in kg): "))
-            height = float(input("Enter your height (in cm): "))
+            print("Please answer the following questions\n")
+            weight = float(input("Enter your current weight (in kg): \n"))
+            height = float(input("Enter your height (in cm): \n"))
             age = int(input("Enter your age: "))
-            desired_weight = float(input("Enter your desired weight (in kg): "))
+            desired_weight = float(input("Enter the weight youd like to build up to (in kg): \n"))
             calories_per_day, time_to_reach_desired_weight = weight_gain_time(weight, height, age, desired_weight)
-            print("To gain weight, you should consume approximately", calories_per_day, "calories per day.")
-            print("It will take approximately {} weeks to reach your desired weight.".format(time_to_reach_desired_weight))
+            print("To gain this weight, you should eat about", ceil(calories_per_day), "calories per day.")
+            print("and will take approximately {} weeks to reach your desired weight.".format(time_to_reach_desired_weight))
             weight_gain_time(weight, height, age, desired_weight)
+            user_data = (weight,height,age,desired_weight)
             break
         else:
             print("Invalid response, please enter either 'lose' or 'gain'")
 
-def weight_loss_time(weight, desired_weight, deficit_per_day):
-    weight_loss_rate = weight - desired_weight
-    #this eqn is from the net
-    deficit_needed = weight_loss_rate * 7700
-    days_needed = deficit_needed / deficit_per_day
-    return days_needed
 
-def weight_gain_time(weight, height, age, desired_weight):
-    # Harris-Benedict equation to calculate Basal Metabolic Rate (BMR)
-    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-    # Multiply BMR by 1.55 to estimate calorie intake needed to gain weight
-    weight_gain_time = BMR * 1.55
-    # Estimate time to reach desired weight (assuming a weight gain of 0.5 kg per week)
-    time_to_reach_desired_weight = (desired_weight - weight) / 0.5
-    return weight_gain_time, time_to_reach_desired_weight
+# is this function storing user data in the spread sheet. ?# 
+# NOOOOOOO #
+def stored_user_data():
+    weight, age , height,desired_weight = weight_change()
+    user_data = (weight,height,age,desired_weight)
+    return user_data,
 
 
 
-weight_change()
+
+# USE THIS TO VALIDATE DATA #
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 4:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+
+
+
+# DECLARE GLOBAL VARIABLES HERE?? #
+ #user_data = (weight,height,age,desired_weight)
+ # maybe define user ddata and weight in another fucntion that does ask the user anything in the terminal 
+
+
+
+
+def main ():
+    draw_jolly_roger()
+    weight_change()
+    #stored_user_data()
+
+            
+   
+
+    
+  # NOTE #
+  # still need to figure out how to change color of text in terminal#
+    
+
+
+#main()
+
+# 5 
+## new code janvier 23 2023 ##
+def exercise_suggestions():
+    exercise_type = input("What type of exercises qre you into?? (cardio, strength, yoga) ")
+    if exercise_type.lower() == "cardio":
+        print("Try going for a run , boxingn or some type of martial arts.")
+    elif exercise_type.lower() == "strength":
+        print("Try lifting weights at the gym")
+    elif exercise_type.lower() == "yoga":
+        print("Try a yoga class, doing a yoga video at home, or going for a yoga hike.")
+    else:
+        print("SORRY   enter something that makes sense wesh.")
+
+# exercise_suggestions()
+# I should get a value here anc store it somehwer# 
+
+
+### I want to take all this data and return a weekly schedlue#
+
+# function 6
+def workout_plan():
+    workout_frequency = input("How often would you like to work out each week?")
+    if "3" in workout_frequency or "three" in workout_frequency:
+        print("A good plan would be to work out Monday, Wednesday, and Friday.")
+    elif "4" in workout_frequency or "four" in workout_frequency:
+        print("A good plan would be to work out Monday, Tuesday, Thursday, and Friday.")
+    elif "5" in workout_frequency or "five" in workout_frequency:
+        print("A good plan would be to work out Monday through Friday.")
+    elif "6" in workout_frequency or "six" in workout_frequency:
+        print("A good plan would be to work out Monday through Saturday.")
+    elif "7" in workout_frequency or "seven" in workout_frequency:
+        print("A good plan would be to work out every day.")
+    else:
+        print("Sorry, I don't understand your input.")
+
+#workout_plan()
+
+def workout_schedule():
+    #Ask user for types of exercises they like
+    exercise_types = input("What types of exercises do you like? (e.g. cardio, strength, yoga) ")
+
+    #Ask user for how many days they would like to work out
+    workout_days = int(input("How many days per week would you like to work out? "))
+
+    #Ask user for their current weight
+    current_weight = float(input("What is your current weight in kg? "))
+
+    #Ask user for their desired weight
+    desired_weight = float(input("What is your desired weight in kg? "))
+
+    #Calculate calorie deficit needed per day to reach desired weight
+    calorie_deficit = (current_weight - desired_weight) * 7700
+
+    #Create empty dictionary for workout schedule
+    schedule = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [], "Saturday": [], "Sunday": []}
+
+    #Populate workout schedule with exercise types
+    for i in range(workout_days):
+        day = input(f"Which day do you want to work out? (e.g. Monday) ")
+        schedule[day.capitalize()].append(exercise_types)
+    # Print the schedule
+    print("Here is your weekly workout schedule:")
+    for day, workout in schedule.items():
+        if workout:
+            print(f"{day}: {workout}")
+        else:
+            print(f"{day}: Rest Day")
+
+
+
+workout_schedule()
+
