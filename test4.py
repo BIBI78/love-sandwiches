5688db3e67f328c086b2ab51414d16e61e3075c6
BIBI78 <blakecollins78@gmail.com>
2023-02-10 16:18:08 +0000

    kinda got funtion to work

--- test4.py
+++ test4.py
@@ -22,12 +22,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('1000_sunny_fitness')
 
+
+## OK NOW I EDIT##
 def get_user_data():
-    weight = float(input("Enter your current weight (in kg): "))
+    print("Please answer the following questions\n")
     age = int(input("Enter your age: "))
     height = float(input("Enter your height (in cm): "))
+    weight = float(input("Enter your current weight (in kg): "))
     desired_weight = float(input("Enter your desired weight (in kg): "))
-    weight_change = input("Do you want to gain weight or lose weight? (Enter 'gain' or 'Lose'): ")
+    weight_change = input("Do you want to gain weight or lose weight? (Enter 'Gain' or 'Lose'): ")
     
     return weight, age, height, desired_weight, weight_change
 
@@ -35,10 +38,20 @@ def calculate_weight_change_time(weight, age, height, desired_weight, weight_cha
     weight_difference = desired_weight - weight
     time_in_weeks = 0
     if weight_change == "Gain":
-        time_in_weeks = weight_difference * 7700 / (height * 6.25)
+         BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+         weight_gain_time = BMR * 1.55
+         time_to_reach_desired_weight = (desired_weight - weight) / 0.5
+         time_needed = (desired_weight - weight) / 0.5
     elif weight_change == "Lose":
-        time_in_weeks = weight_difference * 7700 / (height * 9.25)
-    return time_in_weeks
+        weight_loss_rate = weight - desired_weight
+        deficit_needed = weight_loss_rate * 7700
+        recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
+        deficit_per_day = recomended_deficit 
+        days_needed = deficit_needed / deficit_per_day
+        recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
+        deficit_needed = recomended_deficit
+        time_needed = recomended_deficit
+    return time_needed
 
 def main():
     weight, age, height, desired_weight, weight_change = get_user_data()
